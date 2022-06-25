dc5b45cdc95f8a5f1e76669d2ce937dce87d60ee
BandiKameswararao <105433677+BandiKameswararao@users.noreply.github.com>
2022-06-25 21:54:59 +0530

    Add files via upload
    Discover-service day24

---
+++ DiscoverServer/src/test/java/Discoverserver/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
