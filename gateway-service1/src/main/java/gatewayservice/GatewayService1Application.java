7f9d6bf425e3dc30b3794656115a0e8e44312aef
BandiKameswararao <105433677+BandiKameswararao@users.noreply.github.com>
2022-06-28 11:52:40 +0530

    Gateway service

---
+++ gateway-service1/src/main/java/gatewayservice/GatewayService1Application.java
@@ -0,0 +1,15 @@
+package gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@Enablegatewayservice
+public class GatewayService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayService1Application.class, args);
+	}
+
+}
